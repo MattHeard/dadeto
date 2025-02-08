37fab5d60da6e3958cf6a9af4101dcd21493728a
Matthew Heard <matthew.heard@outfittery.de>
2025-02-08 15:40:57 +0100

    Iterate over posts

--- src/generator.js
+++ src/generator.js
@@ -1,21 +1,11 @@
 const header = `<!DOCTYPE html><html lang="en"><head><title>Matt Heard</title><meta name="viewport" content="width=device-width"><link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Acme&family=Inter:wght@100..900&display=swap" rel="stylesheet"></head><body><header><h1>Matt Heard</h1><p>Software developer and philosopher in Berlin</p></header>`;
-const article = `<article><p>Hello, world!</p></article>`;
 const footer = `<footer>All content is authored by Matt Heard and is <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY-NC-SA 4.0</a>, unless otherwise noted.</footer></body></html>`;
 
+// Create an article from a post
+const getArticle = (post) => `<article><p>${post.content}</p></article>`;
+
+// Generate the HTML from a blog
 export function generateBlog(blog) {
-    if (blog.posts.length === 0) {
-        return header + footer;
-    }
-
-    if (blog.posts.length == 1) {
-        return header + article + footer;
-    }
-
-    if (blog.posts.length == 2) {
-        const firstArticle = `<article><p>First post content</p></article>`;
-        const secondArticle = `<article><p>Second post content</p></article>`;
-        return header + firstArticle + secondArticle + footer;
-    }
-    
-    return header + article + footer;
-}
+    const articles = blog.posts.map(getArticle);
+    return header + articles.join("") + footer;
+};
