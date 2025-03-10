dcfa76e9f49d73e3d316074f02b5c8141671a0ba
Matthew Heard <matthew.heard@outfittery.de>
2025-03-10 11:20:34 +0100

    refactor

--- src/generator.js
+++ src/generator.js
@@ -660,7 +660,7 @@ function generateRelatedLinksSection(post) {
     return '';
   }
 
-  const keyDiv = createDiv(CLASS.KEY, 'related');
+  const keyDiv = createDiv(CLASS.KEY, 'links');
   // Join with an empty string to avoid spacing issues in the HTML output
   const linksList = post.relatedLinks.map(link => formatRelatedLink(link)).join('');
   const valueContent = `<ul class="related-links">${linksList}</ul>`;
