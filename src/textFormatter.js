21cfc372e1f37ab2723ef52daefa767860e7c3dc
Matthew Heard <matthew.heard@outfittery.de>
2025-03-18 12:33:39 +0100

    refactor

--- src/textFormatter.js
+++ src/textFormatter.js
@@ -7,9 +7,9 @@
  * @returns {string} The formatted text.
  */
 export function getFormattedText(text) {
-  if (text.startsWith('**') && text.endsWith('**') && text.length >= 4) {
-    return `<strong>${text}</strong>`;
-  } else if (text.startsWith('__') && text.endsWith('__') && text.length >= 4) {
+  if ((text.startsWith('**') && text.endsWith('**') || 
+       text.startsWith('__') && text.endsWith('__')) && 
+      text.length >= 4) {
     return `<strong>${text}</strong>`;
   }
   return text;
