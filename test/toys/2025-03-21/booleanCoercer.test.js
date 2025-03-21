f073ce910afc0d302d250f4cbcfd8059e057d654
Matthew Heard <matthew.heard@outfittery.de>
2025-03-21 14:55:16 +0100

    refactor

--- test/toys/2025-03-21/booleanCoercer.test.js
+++ test/toys/2025-03-21/booleanCoercer.test.js
@@ -3,42 +3,42 @@ import { coerceToBoolean } from '../../../src/toys/2025-03-21/booleanCoercer.js'
 
 describe('coerceToBoolean', () => {
   test('returns { value: true } for input boolean true', () => {
-    expect(coerceToBoolean(true)).toEqual({ value: true });
+    expect(coerceToBoolean(true)).toBe('{ value: true }');
   });
 
   test('returns { value: false } for input boolean false', () => {
-    expect(coerceToBoolean(false)).toEqual({ value: false });
+    expect(coerceToBoolean(false)).toBe('{ value: false }');
   });
 
   test('returns { value: true } for input string "true"', () => {
-    expect(coerceToBoolean("true")).toEqual({ value: true });
+    expect(coerceToBoolean("true")).toBe('{ value: true }');
   });
 
   test('returns { value: false } for input string "false"', () => {
-    expect(coerceToBoolean("false")).toEqual({ value: false });
+    expect(coerceToBoolean("false")).toBe('{ value: false }');
   });
 
   test('handles case insensitivity for "TrUe"', () => {
-    expect(coerceToBoolean("TrUe")).toEqual({ value: true });
+    expect(coerceToBoolean("TrUe")).toBe('{ value: true }');
   });
 
   test('handles case insensitivity for "FaLsE"', () => {
-    expect(coerceToBoolean("FaLsE")).toEqual({ value: false });
+    expect(coerceToBoolean("FaLsE")).toBe('{ value: false }');
   });
 
   test('returns {} for non-boolean and non-valid string input (number)', () => {
-    expect(coerceToBoolean(123)).toEqual({});
+    expect(coerceToBoolean(123)).toBe('{}');
   });
 
   test('returns {} for non-boolean and non-valid string input (object)', () => {
-    expect(coerceToBoolean({})).toEqual({});
+    expect(coerceToBoolean({})).toBe('{}');
   });
 
   test('returns {} for non-boolean and non-valid string input (null)', () => {
-    expect(coerceToBoolean(null)).toEqual({});
+    expect(coerceToBoolean(null)).toBe('{}');
   });
 
   test('returns {} for non-boolean and non-valid string input (undefined)', () => {
-    expect(coerceToBoolean(undefined)).toEqual({});
+    expect(coerceToBoolean(undefined)).toBe('{}');
   });
 });
