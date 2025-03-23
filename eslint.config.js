e4362d3b67980b7ae1820758a811a4a2c3f0b8da
Matthew Heard <matthew.heard@outfittery.de>
2025-03-23 13:57:06 +0100

    refactor

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import js from "@eslint/js";
 
 
 export default defineConfig([
+  { ignores: ["public/"] },
   { files: ["**/*.{js,mjs,cjs}"] },
   { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
