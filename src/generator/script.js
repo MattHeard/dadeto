4a87aad44740de7d91544fdc3ea4a7b42e2fab6f
Matthew Heard <matthew.heard@outfittery.de>
2025-03-28 18:20:48 +0100

    refactor

--- src/generator/script.js
+++
@@ -1,5 +0,0 @@
-// Script tag that references the external main.js file
-const scriptTag = `<script src="main.js" defer></script>`;
-
-// Export the script tag as the default export
-export default scriptTag;
