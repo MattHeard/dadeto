da0267f46e6840816a671ba57ce70b07920b6b46
Matthew Heard <matthew.heard@outfittery.de>
2025-04-13 15:27:16 +0200

    refactor

--- test/browser/tags.test.js
+++ test/browser/tags.test.js
@@ -1,3 +1,4 @@
+import { jest } from '@jest/globals';
 import { hideArticlesByClass } from '../../src/browser/tags.js';
 
 describe('hideArticlesByClass', () => {
@@ -9,5 +10,20 @@ describe('hideArticlesByClass', () => {
     expect(() => {
       hideArticlesByClass('some-class', getElementsByTagName, hasClassFn, hideElementFn);
     }).not.toThrow();
+  });
+
+  it('hides articles with the given class', () => {
+    const article1 = {};
+    const article2 = {};
+    const articles = [article1, article2];
+
+    const getElementsByTagName = () => articles;
+    const hasClassFn = (el, className) => el === article1 && className === 'target-class';
+    const hideElementFn = jest.fn();
+
+    hideArticlesByClass('target-class', getElementsByTagName, hasClassFn, hideElementFn);
+
+    expect(hideElementFn).toHaveBeenCalledTimes(1);
+    expect(hideElementFn).toHaveBeenCalledWith(article1);
   });
 });
