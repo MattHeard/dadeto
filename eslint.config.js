63e9fc4977265c0c3b8e43db05c8cdd8ac84eecf
Matthew Heard <matthew.heard@outfittery.de>
2025-04-14 20:01:42 +0200

    refactor

--- eslint.config.js
+++ eslint.config.js
@@ -27,6 +27,7 @@ export default [
       "no-duplicate-imports": "warn",
       "no-implicit-coercion": "warn",
       "dot-notation": "warn",
+      "max-depth": ["warn", 4],
       // Add other project-specific rules here if needed
     }
   },
