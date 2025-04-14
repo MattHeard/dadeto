cace44baf99fc33e0da6ff62ed240bf2f3c41373
Matthew Heard <matthew.heard@outfittery.de>
2025-04-14 17:01:53 +0200

    refactor

--- src/toys/2025-04-06/ticTacToe.js
+++ src/toys/2025-04-06/ticTacToe.js
@@ -71,6 +71,31 @@ function determineNextPlayer(moves) {
   return moves[moves.length - 1].player === "X" ? "O" : "X";
 }
 
+function checkRows(board, player) {
+  for (let i = 0; i < 3; i++) {
+    if (board[i][0] === player && board[i][1] === player && board[i][2] === player) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function checkColumns(board, player) {
+  for (let i = 0; i < 3; i++) {
+    if (board[0][i] === player && board[1][i] === player && board[2][i] === player) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function checkDiagonals(board, player) {
+  return (
+    (board[0][0] === player && board[1][1] === player && board[2][2] === player) ||
+    (board[0][2] === player && board[1][1] === player && board[2][0] === player)
+  );
+}
+
 function checkEarlyWin(board) {
   return isWin(board, "X") || isWin(board, "O");
 }
@@ -160,14 +185,8 @@ function validateAndApplyMoves(moves) {
   return { board, earlyWin: false };
 }
 
-function isWin(b, p) {
-  for (let i = 0; i < 3; i++) {
-    if (b[i][0] === p && b[i][1] === p && b[i][2] === p) return true;
-    if (b[0][i] === p && b[1][i] === p && b[2][i] === p) return true;
-  }
-  if (b[0][0] === p && b[1][1] === p && b[2][2] === p) return true;
-  if (b[0][2] === p && b[1][1] === p && b[2][0] === p) return true;
-  return false;
+function isWin(board, player) {
+  return checkRows(board, player) || checkColumns(board, player) || checkDiagonals(board, player);
 }
 
 function returnInitialOptimalMove() {
