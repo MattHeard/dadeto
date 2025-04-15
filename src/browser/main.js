5825858e4408f2bd9a880eb7b0dda9f6f1f36080
Matthew Heard <matthew.heard@outfittery.de>
2025-04-15 16:39:13 +0200

    refactor

--- src/browser/main.js
+++ src/browser/main.js
@@ -7,7 +7,7 @@ let globalState = {
 };
 
 import { setupAudio } from './audio-controls.js';
-import { initializeVisibleComponents, handleModuleError, initialiseModule } from './toys.js';
+import { initializeVisibleComponents } from './toys.js';
 import { hideArticlesByClass, toggleHideLink, createHandleClick } from './tags.js';
 import { fetchAndCacheBlogData, getData, setData } from './data.js';
 import {
