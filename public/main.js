819618d2ac2536553bb2f3ef36e024083cd6c686
Matthew Heard <matthew.heard@outfittery.de>
2025-04-15 09:02:21 +0200

    refactor

--- public/main.js
+++ public/main.js
@@ -51,7 +51,7 @@ const createHandleClick = (dom, link, className) => event => {
     className,
     dom.hasNextSiblingClass,
     dom.removeNextSibling,
-    createHideSpan
+    dom.createHideSpan
   );
 };
 
@@ -126,7 +126,7 @@ const handleTagLinks = () => {
   const handleLink = link => {
     const handleClassName = className => {
       if (className.indexOf('tag-') === 0) {
-        const handleClick = createHandleClick({ stopDefault, hasNextSiblingClass, removeNextSibling }, link, className);
+        const handleClick = createHandleClick({ stopDefault, hasNextSiblingClass, removeNextSibling, createHideSpan }, link, className);
         addEventListener(link, 'click', handleClick);
         return; // exit after first tag- match
       }
