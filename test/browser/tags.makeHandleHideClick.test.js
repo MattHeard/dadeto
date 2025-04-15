5f4d4c8e65515ff3e13298cdfe6f168c3e9bc2fa
Matthew Heard <matthew.heard@outfittery.de>
2025-04-15 19:46:31 +0200

    refactor

---
+++ test/browser/tags.makeHandleHideClick.test.js
@@ -0,0 +1,25 @@
+import { jest } from '@jest/globals';
+import { makeHandleHideClick } from '../../src/browser/tags.js';
+
+describe('makeHandleHideClick', () => {
+  it('calls stopDefault and hides only articles with the given class', () => {
+    const article1 = { classList: { contains: (cls) => cls === 'foo' } };
+    const article2 = { classList: { contains: () => false } };
+    const articles = [article1, article2];
+    const stopDefault = jest.fn();
+    const hide = jest.fn();
+    const dom = {
+      stopDefault,
+      getElementsByTagName: jest.fn(() => articles),
+      hasClass: (el, className) => el.classList.contains(className),
+      hide
+    };
+    const handler = makeHandleHideClick(dom, 'foo');
+    const fakeEvent = {};
+    handler(fakeEvent);
+    expect(stopDefault).toHaveBeenCalledWith(fakeEvent);
+    expect(hide).toHaveBeenCalledTimes(1);
+    expect(hide).toHaveBeenCalledWith(article1);
+    expect(hide).not.toHaveBeenCalledWith(article2);
+  });
+});
