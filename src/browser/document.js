12ae7220b401417ef5ede4d24a709e5a4062a4fe
Matthew Heard <matthew.heard@outfittery.de>
2025-04-26 08:54:32 +0200

    refactor

--- src/browser/document.js
+++ src/browser/document.js
@@ -8,7 +8,7 @@ export const removeControlsAttribute = (audio) => audio.removeAttribute("control
 export const createTextNode = (value) => document.createTextNode(value);
 export const getElementsByTagName = (tagName) => document.getElementsByTagName(tagName);
 export const hasClass = (element, cls) => element.classList.contains(cls);
-export const hide = (element) => { element.style.display = 'none'; };
+export const hide = (element) => element.style.display = 'none';
 export const addEventListener = (element, event, func) => element.addEventListener(event, func);
 export const appendChild = (parentNode, newChild) => parentNode.appendChild(newChild);
 export const insertBefore = (parentNode, newChild, refChild) => parentNode.insertBefore(newChild, refChild);
@@ -46,13 +46,8 @@ export const hasNextSiblingClass = (link, cls) =>
   link.nextElementSibling && link.nextElementSibling.classList.contains(cls);
 
 // DOM manipulation functions
-export const addWarning = (outputElement) => {
-  outputElement.classList.add('warning');
-};
-
-export const removeWarning = (outputElement) => {
-  outputElement.classList.remove('warning');
-};
+export const addWarning = (outputElement) => outputElement.classList.add('warning');
+export const removeWarning = (outputElement) => outputElement.classList.remove('warning');
 
 // Enables the given input element by setting its disabled property to false
 export function enable(input) {
