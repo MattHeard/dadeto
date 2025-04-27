e3410c206e432ebcbf280a6bcf81ef6c2675f095
Matthew Heard <matthew.heard@outfittery.de>
2025-04-27 15:41:58 +0200

    refactor

--- test/browser/toys.test.js
+++ test/browser/toys.test.js
@@ -1,5 +1,5 @@
 import { describe, it, expect, beforeEach, jest } from '@jest/globals';
-import { makeObserverCallback, makeCreateIntersectionObserver, enableInteractiveControls, initialiseModule, initializeAndRenderComponent } from '../../src/browser/toys.js';
+import { makeObserverCallback, makeCreateIntersectionObserver, enableInteractiveControls, initializeAndRenderComponent } from '../../src/browser/toys.js';
 
 describe('makeObserverCallback', () => {
   let importModule, disconnectObserver, f, modulePath, entry, observer;
