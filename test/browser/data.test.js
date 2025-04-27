4ed7af79f529a4e2311a1f6e1f301636209135c8
Matthew Heard <matthew.heard@outfittery.de>
2025-04-27 13:01:59 +0200

    refactor

--- test/browser/data.test.js
+++ test/browser/data.test.js
@@ -1,5 +1,5 @@
 import { describe, it, expect, beforeEach, jest } from '@jest/globals';
-import { fetchAndCacheBlogData, getData, getData_new, setData, getDeepStateCopy, shouldUseExistingFetch } from '../../src/browser/data.js';
+import { fetchAndCacheBlogData, getData_new, setData, getDeepStateCopy, shouldUseExistingFetch } from '../../src/browser/data.js';
 
 describe('fetchAndCacheBlogData', () => {
   let state;
