ebeebea88e2b30e1ac668b44a07c13d57fcf5b8c
Matthew Heard <matthew.heard@outfittery.de>
2025-04-27 15:24:09 +0200

    refactor

--- test/browser/toys.test.js
+++ test/browser/toys.test.js
@@ -1,5 +1,6 @@
 import { describe, it, expect, beforeEach, jest } from '@jest/globals';
 import * as toysModule from '../../src/browser/toys.js';
+import { makeObserverCallback } from '../../src/browser/toys.js';
 
 describe('makeObserverCallback', () => {
   let importModule, disconnectObserver, f, modulePath, entry, observer;
