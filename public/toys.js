808652f5cf7d1d6d3ccc989bc39179e3de8a6975
Matthew Heard <matthew.heard@outfittery.de>
2025-04-28 21:01:44 +0200

    refactor

--- public/toys.js
+++ public/toys.js
@@ -162,7 +162,8 @@ export function makeCreateIntersectionObserver(dom, env) {
  * @param {object} dom - DOM helper object
  * @param {HTMLElement} parent
  */
-export function enableInteractiveControls(inputElement, submitButton, dom, parent) {
+export function enableInteractiveControls(elements, submitButton, dom, parent) {
+  const { inputElement } = elements;
   dom.enable(inputElement);
   dom.enable(submitButton);
   setTextContent('Ready for input', dom, parent);
@@ -337,7 +338,7 @@ export function initializeInteractiveComponent(article, processingFunction, conf
   dom.addEventListener(inputElement, 'keypress', createHandleKeyPress(handleSubmit));
 
   // Enable controls when initialization is complete using the function from this module
-  enableInteractiveControls(inputElement, submitButton, dom, outputParent);
+  enableInteractiveControls({ inputElement }, submitButton, dom, outputParent);
 }
 
 /**
