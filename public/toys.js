d56e9697de5ece865f5d8baeace39e1836b81bf2
Matthew Heard <matthew.heard@outfittery.de>
2025-05-02 19:49:26 +0200

    refactor

--- public/toys.js
+++ public/toys.js
@@ -272,13 +272,13 @@ function createHandleInputError(env, parent) {
 
 function processInputAndSetOutput(elements, processingFunction, env) {
   const { inputElement, outputParentElement: parent } = elements;
-  const { globalState, createEnv } = env;
+  const { globalState, createEnv, dom } = env;
   const toyEnv = createEnv(globalState);
   const inputValue = inputElement.value;
   const result = processingFunction(inputValue, toyEnv);
   const parsed = parseJSONResult(result);
   if (!handleParsedResult_new(parsed, parent, env)) {
-    setTextContent(result, env.dom, parent);
+    setTextContent(result, dom, parent);
   }
 }
 
