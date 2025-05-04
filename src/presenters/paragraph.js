3241525954152473bd8613379273782d02e92928
Matthew Heard <matthew.heard@outfittery.de>
2025-05-04 09:15:05 +0200

    refactor

--- src/presenters/paragraph.js
+++ src/presenters/paragraph.js
@@ -1,11 +1,15 @@
+function setParagraphText(paragraph, inputString, dom) {
+  if (dom && typeof dom.setTextContent === 'function') {
+    dom.setTextContent(paragraph, inputString);
+  } else {
+    paragraph.textContent = inputString;
+  }
+}
+
 export function createParagraphElement(inputString, dom) {
   const paragraph = dom.createElement('p');
   if (paragraph) {
-    if (dom && typeof dom.setTextContent === 'function') {
-      dom.setTextContent(paragraph, inputString);
-    } else {
-      paragraph.textContent = inputString;
-    }
+    setParagraphText(paragraph, inputString, dom);
   }
   return paragraph;
 }
