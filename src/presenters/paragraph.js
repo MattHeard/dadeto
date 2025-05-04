e54dce4ac627c372f312ff941ccc6a39acca0859
Matthew Heard <matthew.heard@outfittery.de>
2025-05-04 09:17:26 +0200

    refactor

--- src/presenters/paragraph.js
+++ src/presenters/paragraph.js
@@ -1,9 +1,5 @@
-function setParagraphText(paragraph, inputString, dom) {
-  dom.setTextContent(paragraph, inputString);
-}
-
 export function createParagraphElement(inputString, dom) {
   const paragraph = dom.createElement('p');
-  setParagraphText(paragraph, inputString, dom);
+  dom.setTextContent(paragraph, inputString);
   return paragraph;
 }
