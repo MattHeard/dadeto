969fe02a7794266c4bff3887ee044ed5b402be19
Matthew Heard <matthew.heard@outfittery.de>
2025-05-08 11:59:09 +0200

    refactor

---
+++ public/2025-03-29/setOutput.js
@@ -0,0 +1,37 @@
+export function setOutput(input, env) {
+  let inputJson;
+  try {
+    inputJson = JSON.parse(input);
+  } catch (parseError) {
+    return `Error: Invalid JSON input. ${parseError.message}`;
+  }
+  return processSetOutput(inputJson, env);
+}
+
+function processSetOutput(inputJson, env) {
+  if (!isObject(inputJson)) {
+    return "Error: Input JSON must be a plain object.";
+  }
+  return handleValidOutputInput(inputJson, env);
+}
+
+function handleValidOutputInput(inputJson, env) {
+  const getData = env.get('getData');
+  const setData = env.get('setData');
+  try {
+    return mergeOutputData(getData, setData, inputJson);
+  } catch (error) {
+    return `Error updating output data: ${error.message}`;
+  }
+}
+
+function mergeOutputData(getData, setData, inputJson) {
+  const currentData = getData();
+  const newData = JSON.parse(JSON.stringify(currentData));
+  if (!isObject(newData.output)) {
+    newData.output = {};
+  }
+  newData.output = deepMerge(newData.output, inputJson);
+  setData(newData);
+  return `Success: Output data deep merged.`;
+}
