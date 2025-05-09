bd30957451921ea3489691f8d2ecf04f1f1cb054
Matthew Heard <matthew.heard@outfittery.de>
2025-05-09 15:19:57 +0200

    refactor

--- public/presenters/battleshipSolitaireFleet.js
+++ public/presenters/battleshipSolitaireFleet.js
@@ -73,7 +73,7 @@ export function createBattleshipFleetBoardElement(inputString, dom) {
 }
 
 function renderFleetBoard(fleet, dom) {
-  const { width, height, ships } = fleet;
+  const { width, height } = fleet;
   // 2. Initialise empty grid with water symbols
   const createWaterRow = () => Array(width).fill('\u00b7');
   const board = Array.from({ length: height }, createWaterRow);
