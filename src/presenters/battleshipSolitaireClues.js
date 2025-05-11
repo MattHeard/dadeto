45530e55fbbed81ac6ee111d4206992dc7dc0703
Matthew Heard <matthew.heard@outfittery.de>
2025-05-11 20:40:56 +0200

    refactor

---
+++ src/presenters/battleshipSolitaireClues.js
@@ -0,0 +1,8 @@
+// Presenter for Battleship Solitaire Clues
+// This file is intentionally left mostly empty for future expansion.
+// Export a default empty function for now.
+
+export default function BattleshipSolitaireCluesPresenter() {
+  // TODO: Implement presenter logic for displaying battleship clues
+  return null;
+}
