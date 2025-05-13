3b2bf958ab463fdc235af4f119561a3f62a417c4
Matthew Heard <matthew.heard@outfittery.de>
2025-05-13 12:58:09 +0200

    refactor

---
+++ public/2025-05-13/base64Encoder.js
@@ -0,0 +1,8 @@
+// Toy: Base64 Encoder
+// Exports a function that takes a string input and returns its Base64 encoding.
+// This is an empty stub for now.
+
+export function encodeBase64(input) {
+  // TODO: Implement encoding logic
+  return '';
+}
