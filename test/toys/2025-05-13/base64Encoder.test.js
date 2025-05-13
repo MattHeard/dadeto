17942600692ccfc4025c92f5267215eec05f0483
Matthew Heard <matthew.heard@outfittery.de>
2025-05-13 14:30:32 +0200

    refactor

--- test/toys/2025-05-13/base64Encoder.test.js
+++ test/toys/2025-05-13/base64Encoder.test.js
@@ -3,7 +3,13 @@ import { jest } from '@jest/globals';
 
 describe('encodeBase64', () => {
   it('encodes a string to Base64 using the provided environment function', () => {
-    const encoder = jest.fn(() => 'dGVzdA==');
+    const encoder = jest.fn((input) => {
+      if (input === 'test') {
+        return 'dGVzdA==';
+      } else {
+        return 'unexpected input';
+      }
+    });
     const env = { encodeBase64: encoder };
     expect(encodeBase64('test', env)).toBe('dGVzdA==');
   });
