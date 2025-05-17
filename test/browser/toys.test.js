fd3d66c525c80f8765ec987ff88e2cac80fe0ac1
Matthew Heard <matthew.heard@outfittery.de>
2025-05-17 11:19:54 +0200

    refactor

--- test/browser/toys.test.js
+++ test/browser/toys.test.js
@@ -856,7 +856,7 @@ describe('toys', () => {
 describe('createInputDropdownHandler', () => {
   it('handles input dropdown changes', () => {
     // Arrange
-    const getCurrentTarget = jest.fn();
+    const getCurrentTarget = jest.fn().mockReturnValue(null);
     const dom = {
       getCurrentTarget,
       getParentElement: jest.fn(),
