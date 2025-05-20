9261895bb9174fae10d30df71ae823199e69e3c2
Matthew Heard <matthew.heard@outfittery.de>
2025-05-20 15:57:45 +0200

    refactor

--- src/browser/toys.js
+++ src/browser/toys.js
@@ -794,7 +794,7 @@ export const ensureKeyValueInput = (container, textInput, dom) => {
     }
 
     const entries = Object.entries(rows);
-    entries.forEach(([key, value], idx) => {
+    const createRow = ([key, value], idx) => {
       const rowEl = dom.createElement('div');
       dom.setClassName(rowEl, 'kv-row');
 
@@ -811,6 +811,11 @@ export const ensureKeyValueInput = (container, textInput, dom) => {
       disposers.push(() => dom.removeEventListener(keyEl, 'input', onKey));
 
       // Value field
+      return { rowEl, keyEl };
+    };
+
+    entries.forEach((entry, idx) => {
+      const { rowEl, keyEl } = createRow(entry, idx);
       const valueEl = dom.createElement('input');
       dom.setType(valueEl, 'text');
       dom.setPlaceholder(valueEl, 'Value');
