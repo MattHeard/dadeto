2c1ac58a4c58ad4d4bc4578a593880c12b566f9c
Matthew Heard <matthew.heard@outfittery.de>
2025-05-26 21:50:08 +0200

    refactor

--- test/browser/toys.createRenderer.test.js
+++ test/browser/toys.createRenderer.test.js
@@ -1,8 +1,37 @@
+import { jest } from '@jest/globals';
 import { createRenderer } from '../../src/browser/toys.js';
 
 describe('createRenderer', () => {
   it('should create a renderer function', () => {
     // Test case without assertions to verify the function can be called
-    const renderer = createRenderer();
+    const removeAllChildren = jest.fn();
+    const createElement = jest.fn();
+    const setClassName = jest.fn();
+    const setType = jest.fn();
+    const setPlaceholder = jest.fn();
+    const setValue = jest.fn();
+    const setDataAttribute = jest.fn();
+    const addEventListener = jest.fn();
+    const setTextContent = jest.fn();
+    const appendChild = jest.fn();
+    const dom = {
+      removeAllChildren,
+      createElement,
+      setClassName,
+      setType,
+      setPlaceholder,
+      setValue,
+      setDataAttribute,
+      addEventListener,
+      setTextContent,
+      appendChild
+    };
+    const disposers = [];
+    const container = undefined;
+    const rows = { foo: 'bar' };
+    const textInput = undefined;
+    const syncHiddenField = jest.fn();
+    const render = createRenderer(dom, disposers, container, rows, textInput, syncHiddenField);
+    render();
   });
 });
