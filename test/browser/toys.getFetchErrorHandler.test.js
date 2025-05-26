ab414ebda13ff75d61907840e5f29f56bc9bbe3a
Matthew Heard <matthew.heard@outfittery.de>
2025-05-26 08:59:46 +0200

    refactor

--- test/browser/toys.getFetchErrorHandler.test.js
+++ test/browser/toys.getFetchErrorHandler.test.js
@@ -1,9 +1,25 @@
+import { describe, it, jest } from '@jest/globals';
 import { getFetchErrorHandler } from '../../src/browser/toys.js';
 
 describe('getFetchErrorHandler', () => {
   it('can be invoked with no args', () => {
-    // This test intentionally calls the function with no arguments
-    // and has no assertions
-    getFetchErrorHandler();
+    const removeAllChildren = jest.fn();
+    const createElement = jest.fn();
+    const setTextContent = jest.fn();
+    const appendChild = jest.fn();
+    const addWarning = jest.fn();
+
+    const dom = {
+      removeAllChildren,
+      createElement,
+      setTextContent,
+      appendChild,
+      addWarning
+    };
+
+    const presenterKey = 'text';
+    const errorHandler = getFetchErrorHandler({ dom }, null, presenterKey);
+    const error = {};
+    errorHandler(error);
   });
 });
