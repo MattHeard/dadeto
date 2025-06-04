d3e7be708b7a6b2c97bfbcc7778334b4f94b7f71
Matt <matt@mattheard.net>
2025-06-04 12:39:25 +0200

    Add tests to restore 100% branch coverage

--- test/generator/html.test.js
+++ test/generator/html.test.js
@@ -6,7 +6,21 @@ describe('html utilities', () => {
     expect(createOpeningTag('div', '')).toBe('<div>');
   });
 
+  test('createOpeningTag uses default attributes when omitted', () => {
+    expect(createOpeningTag('p')).toBe('<p>');
+  });
+
   test('createTag handles empty attributes', () => {
     expect(createTag('span', '', 'x')).toBe('<span>x</span>');
+  });
+
+  test('createOpeningTag adds space when attributes present', () => {
+    expect(createOpeningTag('div', 'class="c"')).toBe('<div class="c">');
+  });
+
+  test('createTag handles non-empty attributes', () => {
+    expect(createTag('span', 'class="y"', 'x')).toBe(
+      '<span class="y">x</span>'
+    );
   });
 });
