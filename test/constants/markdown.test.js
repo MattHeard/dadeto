5da1d06483b0f713e4fd82e4bb71a120682a25e7
Matt <matt@mattheard.net>
2025-06-04 22:13:35 +0200

    test: verify additional markdown constants

--- test/constants/markdown.test.js
+++ test/constants/markdown.test.js
@@ -12,6 +12,15 @@ describe('markdown constants', () => {
     expect(HTML_TAGS.PRE).toBe('pre');
   });
 
+  test('HTML_TAGS includes list and rule tags', () => {
+    expect(HTML_TAGS.LIST).toBe('ul');
+    expect(HTML_TAGS.LIST_ITEM).toBe('li');
+    expect(HTML_TAGS.ORDERED_LIST).toBe('ol');
+    expect(HTML_TAGS.HORIZONTAL_RULE).toBe('hr');
+    expect(HTML_TAGS.LINE_BREAK).toBe('br');
+    expect(HTML_TAGS.IMAGE).toBe('img');
+  });
+
   test('DEFAULT_OPTIONS has correct default values', () => {
     expect(DEFAULT_OPTIONS.breaks).toBe(false);
     expect(DEFAULT_OPTIONS.sanitize).toBe(false);
