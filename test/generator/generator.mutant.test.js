086fa4606e5c7f5c638a25d89d563bd0abfe6ddb
Matt <matt@mattheard.net>
2025-06-05 21:55:47 +0200

    Use static import for mutant check

--- test/generator/generator.mutant.test.js
+++ test/generator/generator.mutant.test.js
@@ -1,14 +1,12 @@
-import { describe, test, expect, beforeAll } from '@jest/globals';
-
-let generateBlogOuter;
-
-beforeAll(async () => {
-  ({ generateBlogOuter } = await import('../../src/generator/generator.js'));
-});
+import { describe, test, expect } from '@jest/globals';
+import { generateBlogOuter } from '../../src/generator/generator.js';
 
 describe('generator mutants', () => {
   test('output does not contain mutation marker', () => {
     const html = generateBlogOuter({ posts: [] });
     expect(html.includes('Stryker was here!')).toBe(false);
+    expect(html).toContain(
+      '</div></div></div><script type="module" src="browser/main.js" defer></script></body>'
+    );
   });
 });
