67cfa2417bfc3bc6666cb45ed440b5927d33de46
Matt <matt@mattheard.net>
2025-06-05 21:55:34 +0200

    Add test verifying container close before script tag

--- test/generator/footerScript.test.js
+++ test/generator/footerScript.test.js
@@ -4,6 +4,15 @@ import { generateBlogOuter } from '../../src/generator/generator.js';
 describe('footer script tag', () => {
   test('generateBlogOuter includes main script tag', () => {
     const html = generateBlogOuter({ posts: [] });
-    expect(html).toContain('<script type="module" src="browser/main.js" defer></script>');
+    expect(html).toContain(
+      '<script type="module" src="browser/main.js" defer></script>'
+    );
+  });
+
+  test('container is closed before the main script tag', () => {
+    const html = generateBlogOuter({ posts: [] });
+    expect(html).toContain(
+      '</div><script type="module" src="browser/main.js" defer></script>'
+    );
   });
 });
