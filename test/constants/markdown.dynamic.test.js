276a498a182af811f2ff236779fb1ca0ddbaee37
Matt <matt@mattheard.net>
2025-06-05 06:54:35 +0200

    test: verify CSS_CLASSES list item

--- test/constants/markdown.dynamic.test.js
+++ test/constants/markdown.dynamic.test.js
@@ -7,9 +7,10 @@ describe('markdown constants isolated', () => {
   test('HTML_TAGS values via dynamic import', async () => {
     jest.resetModules();
     const module = await import('../../src/constants/markdown.js');
-    const { HTML_TAGS } = module;
+    const { HTML_TAGS, CSS_CLASSES } = module;
     expect(HTML_TAGS.LIST).toBe('ul');
     expect(HTML_TAGS.LIST_ITEM).toBe('li');
     expect(HTML_TAGS.HORIZONTAL_RULE).toBe('hr');
+    expect(CSS_CLASSES.LIST_ITEM).toBe('markdown-list-item');
   });
 });
