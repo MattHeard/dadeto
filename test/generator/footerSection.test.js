6ce09d8d8d6854bce2fffbce1f0e7529c04df369
Matt <matt@mattheard.net>
2025-06-05 21:34:44 +0200

    Add footer section test

---
+++ test/generator/footerSection.test.js
@@ -0,0 +1,14 @@
+import { beforeAll, describe, it, expect } from '@jest/globals';
+
+let generateBlogOuter;
+
+beforeAll(async () => {
+  ({ generateBlogOuter } = await import('../../src/generator/generator.js'));
+});
+
+describe('footer section', () => {
+  it('renders empty key div', () => {
+    const html = generateBlogOuter({ posts: [] });
+    expect(html).toContain('<div class="key"></div><div class="footer value warning">');
+  });
+});
