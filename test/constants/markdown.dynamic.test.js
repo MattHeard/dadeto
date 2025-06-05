ba22e95d3aeea9f07ee36a47f8a10ff576015d09
Matt <matt@mattheard.net>
2025-06-05 06:20:17 +0200

    Add isolated test for markdown constants

---
+++ test/constants/markdown.dynamic.test.js
@@ -0,0 +1,15 @@
+import { describe, test, expect, jest } from '@jest/globals';
+
+/**
+ * Dynamic import tests to ensure constants survive module cache resets.
+ */
+describe('markdown constants isolated', () => {
+  test('HTML_TAGS values via dynamic import', async () => {
+    jest.resetModules();
+    const module = await import('../../src/constants/markdown.js');
+    const { HTML_TAGS } = module;
+    expect(HTML_TAGS.LIST).toBe('ul');
+    expect(HTML_TAGS.LIST_ITEM).toBe('li');
+    expect(HTML_TAGS.HORIZONTAL_RULE).toBe('hr');
+  });
+});
