dd74612162e60357c259cfbc89e06ea2079c9b45
Matt <matt@mattheard.net>
2025-06-05 21:55:26 +0200

    test: ensure container div closes before footer script

--- test/generator/footerScript.test.js
+++ test/generator/footerScript.test.js
@@ -6,4 +6,11 @@ describe('footer script tag', () => {
     const html = generateBlogOuter({ posts: [] });
     expect(html).toContain('<script type="module" src="browser/main.js" defer></script>');
   });
+
+  test('generateBlogOuter closes container before script tag', () => {
+    const html = generateBlogOuter({ posts: [] });
+    const closingDivs = html.match(/<\/div>/g) || [];
+    expect(closingDivs.length).toBe(9);
+    expect(html).toContain('</div><script type="module" src="browser/main.js" defer></script>');
+  });
 });
