95d3bd77c230800a0ad59ec32f141e8f9791b509
Matt <matt@mattheard.net>
2025-06-05 21:55:19 +0200

    Add test for createValueDiv filtering

---
+++ test/generator/createValueDiv.filter.test.js
@@ -0,0 +1,24 @@
+import fs from 'fs';
+import path from 'path';
+import { pathToFileURL } from 'url';
+import { beforeAll, describe, test, expect } from '@jest/globals';
+
+let createValueDiv;
+
+beforeAll(async () => {
+  const generatorPath = path.join(process.cwd(), 'src/generator/generator.js');
+  let src = fs.readFileSync(generatorPath, 'utf8');
+  src = src.replace(/from '\.\/(.*?)'/g, (_, p) => {
+    const absolute = pathToFileURL(path.join(path.dirname(generatorPath), p));
+    return `from '${absolute.href}'`;
+  });
+  src += '\nexport { createValueDiv };';
+  ({ createValueDiv } = await import(`data:text/javascript,${encodeURIComponent(src)}`));
+});
+
+describe('createValueDiv falsey classes', () => {
+  test('filters out falsey additional classes', () => {
+    const html = createValueDiv('content', ['', undefined, 'extra']);
+    expect(html).toBe('<div class="value extra">content</div>');
+  });
+});
