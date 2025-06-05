c7b0d88547c302f1ee10d49c027f9331a5256426
Matt <matt@mattheard.net>
2025-06-05 06:54:39 +0200

    Add test covering generator constants

---
+++ test/generator/generator.constants.test.js
@@ -0,0 +1,12 @@
+import { describe, test, expect } from '@jest/globals';
+import { generateBlogOuter } from '../../src/generator/generator.js';
+
+describe('generator constants usage', () => {
+  test('blog output includes container id and footer classes', () => {
+    const html = generateBlogOuter({ posts: [] });
+    expect(html).toContain('id="container"');
+    expect(html).toContain('class="footer value warning"');
+    expect(html).toContain('All content is authored by Matt Heard');
+    expect(html).toContain('Software developer and philosopher in Berlin');
+  });
+});
