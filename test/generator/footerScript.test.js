684d1925530777c280722861c8428ae1a6c14619
Matt <matt@mattheard.net>
2025-06-05 19:44:20 +0200

    test: verify script tag in blog output

---
+++ test/generator/footerScript.test.js
@@ -0,0 +1,9 @@
+import { describe, test, expect } from '@jest/globals';
+import { generateBlogOuter } from '../../src/generator/generator.js';
+
+describe('footer script tag', () => {
+  test('generateBlogOuter includes main script tag', () => {
+    const html = generateBlogOuter({ posts: [] });
+    expect(html).toContain('<script type="module" src="browser/main.js" defer></script>');
+  });
+});
