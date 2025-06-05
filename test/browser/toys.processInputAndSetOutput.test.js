2988682a1a54b82b8ff7f442a3a77625ca2bf8d6
Matt <matt@mattheard.net>
2025-06-05 06:20:10 +0200

    Add tests for DOM queries in component init

--- test/browser/toys.processInputAndSetOutput.test.js
+++ test/browser/toys.processInputAndSetOutput.test.js
@@ -146,7 +146,12 @@ describe('processInputAndSetOutput', () => {
     const article = { id: 'post1' };
     const outputSelect = { value: 'text' };
     const outputParentElement = {};
-    const elements = { inputElement, article, outputSelect, outputParentElement };
+    const elements = {
+      inputElement,
+      article,
+      outputSelect,
+      outputParentElement,
+    };
     const result = '{"request":{"url":""}}';
     const processingFunction = jest.fn(() => result);
     const setData = jest.fn();
@@ -161,7 +166,9 @@ describe('processInputAndSetOutput', () => {
       createElement: jest.fn(() => ({})),
       appendChild: jest.fn(),
     };
-    const fetchFn = jest.fn(() => Promise.resolve({ text: () => Promise.resolve('') }));
+    const fetchFn = jest.fn(() =>
+      Promise.resolve({ text: () => Promise.resolve('') })
+    );
     const env = { createEnv, dom, fetchFn };
 
     processInputAndSetOutput(elements, processingFunction, env);
