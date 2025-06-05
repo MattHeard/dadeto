867ee31a25844a4f9fad7e7a363857c454177425
Matt <matt@mattheard.net>
2025-06-05 14:19:15 +0200

    Add test for null content normalization

---
+++ test/generator/normalizeContentItem.null.test.js
@@ -0,0 +1,19 @@
+import { describe, test, expect } from '@jest/globals';
+import { generateBlogOuter } from '../../src/generator/generator.js';
+
+describe('normalizeContentItem with null content', () => {
+  test('generateBlogOuter handles null in post content array', () => {
+    const blog = {
+      posts: [
+        {
+          key: 'NULL',
+          title: 'Null Post',
+          publicationDate: '2024-06-01',
+          content: [null],
+        },
+      ],
+    };
+    const html = generateBlogOuter(blog);
+    expect(html).toContain('<p class="value">null</p>');
+  });
+});
