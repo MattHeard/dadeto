0ab2364930c929cd1deebd34c60ec4b3197aa2b2
Matt <matt@mattheard.net>
2025-06-06 18:40:26 +0200

    Add test for prefixIfPresent with falsy values

---
+++ test/generator/prefixIfPresent.test.js
@@ -0,0 +1,25 @@
+import fs from 'fs';
+import path from 'path';
+import { pathToFileURL } from 'url';
+import { beforeAll, describe, test, expect } from '@jest/globals';
+
+let prefixIfPresent;
+
+beforeAll(async () => {
+  const generatorPath = path.join(process.cwd(), 'src/generator/generator.js');
+  let src = fs.readFileSync(generatorPath, 'utf8');
+  src = src.replace(/from '\.\/(.*?)'/g, (_, p) => {
+    const absolute = pathToFileURL(path.join(path.dirname(generatorPath), p));
+    return `from '${absolute.href}'`;
+  });
+  src += '\nexport { prefixIfPresent };';
+  ({ prefixIfPresent } = await import(`data:text/javascript,${encodeURIComponent(src)}`));
+});
+
+describe('prefixIfPresent', () => {
+  test('returns empty string when value is falsy', () => {
+    expect(prefixIfPresent(' by ', '')).toBe('');
+    expect(prefixIfPresent(' by ', undefined)).toBe('');
+    expect(prefixIfPresent(' by ', null)).toBe('');
+  });
+});
