4ea7a191efad2deb609be12a129bdeee8167ab65
Matt <matt@mattheard.net>
2025-06-06 18:36:43 +0200

    Add internal tests for ticTacToeBoard

---
+++ test/presenters/ticTacToeBoard.internal.test.js
@@ -0,0 +1,30 @@
+import fs from 'fs';
+import path from 'path';
+import { pathToFileURL } from 'url';
+import { beforeAll, describe, it, expect } from '@jest/globals';
+
+let getPlayer;
+let getPosition;
+
+beforeAll(async () => {
+  const srcPath = path.join(process.cwd(), 'src/presenters/ticTacToeBoard.js');
+  let src = fs.readFileSync(srcPath, 'utf8');
+  src = src.replace(/from '((?:\.\.?\/).*?)'/g, (_, p) => {
+    const abs = pathToFileURL(path.join(path.dirname(srcPath), p));
+    return `from '${abs.href}'`;
+  });
+  src += '\nexport { getPlayer, getPosition };';
+  ({ getPlayer, getPosition } = await import(
+    `data:text/javascript,${encodeURIComponent(src)}`
+  ));
+});
+
+describe('ticTacToeBoard internal functions', () => {
+  it('getPlayer returns undefined for undefined move', () => {
+    expect(getPlayer(undefined)).toBeUndefined();
+  });
+
+  it('getPosition returns undefined for undefined move', () => {
+    expect(getPosition(undefined)).toBeUndefined();
+  });
+});
