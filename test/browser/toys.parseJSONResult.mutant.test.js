77b9e4b94a5dec8fbadc78dc41bf66507f2c0e87
Matt <matt@mattheard.net>
2025-06-06 18:40:27 +0200

    Use require.resolve in mutant test

--- test/browser/toys.parseJSONResult.mutant.test.js
+++ test/browser/toys.parseJSONResult.mutant.test.js
@@ -1,10 +1,12 @@
 import { describe, it, expect } from '@jest/globals';
 import fs from 'fs';
-import path from 'path';
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
 
 describe('parseJSONResult mutant', () => {
   it('returns null when JSON parsing fails', () => {
-    const filePath = path.join(process.cwd(), 'src/browser/toys.js');
+    const filePath = require.resolve('../../src/browser/toys.js');
     const fileContents = fs.readFileSync(filePath, 'utf8');
     const match = fileContents.match(/function parseJSONResult\([^]*?\n\}/);
     const parseJSONResult = eval('(' + match[0] + ')');
