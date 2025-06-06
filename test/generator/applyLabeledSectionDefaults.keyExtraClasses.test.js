23cf02f4a10acda3aa20f21ccd61368d25153d60
Matt <matt@mattheard.net>
2025-06-06 18:37:11 +0200

    refactor tests to import generator module directly

--- test/generator/applyLabeledSectionDefaults.keyExtraClasses.test.js
+++ test/generator/applyLabeledSectionDefaults.keyExtraClasses.test.js
@@ -1,22 +1,5 @@
-import fs from 'fs';
-import path from 'path';
-import { pathToFileURL } from 'url';
-import { beforeAll, describe, test, expect } from '@jest/globals';
-
-let applyLabeledSectionDefaults;
-
-beforeAll(async () => {
-  const generatorPath = path.join(process.cwd(), 'src/generator/generator.js');
-  let src = fs.readFileSync(generatorPath, 'utf8');
-  src = src.replace(/from '\.\/(.*?)'/g, (_, p) => {
-    const absolute = pathToFileURL(path.join(path.dirname(generatorPath), p));
-    return `from '${absolute.href}'`;
-  });
-  src += '\nexport { applyLabeledSectionDefaults };';
-  ({ applyLabeledSectionDefaults } = await import(
-    `data:text/javascript,${encodeURIComponent(src)}`
-  ));
-});
+import { describe, test, expect } from '@jest/globals';
+import { applyLabeledSectionDefaults } from '../../src/generator/generator.js';
 
 describe('applyLabeledSectionDefaults', () => {
   test('sets default keyExtraClasses', () => {
