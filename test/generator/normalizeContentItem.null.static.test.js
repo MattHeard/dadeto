8030937ea2d29cc1019cd8ab72d75b9b7177f8d4
Matt <matt@mattheard.net>
2025-06-07 21:50:42 +0200

    Add static test for normalizeContentItem

---
+++ test/generator/normalizeContentItem.null.static.test.js
@@ -0,0 +1,19 @@
+import { describe, test, expect } from '@jest/globals';
+import { generateBlogOuter } from '../../src/generator/generator.js';
+
+describe('normalizeContentItem null static import', () => {
+  test('generateBlogOuter handles null in post content array', () => {
+    const blog = {
+      posts: [
+        {
+          key: 'NULL',
+          title: 'Null Post',
+          publicationDate: '2024-06-01',
+          content: [null],
+        },
+      ],
+    };
+    const html = generateBlogOuter(blog);
+    expect(html).toContain('<p class="value">null</p>');
+  });
+});
