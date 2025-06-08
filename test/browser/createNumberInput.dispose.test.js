f2a352a9d11e72d827470cd8adb257eede1221f5
Matt <matt@mattheard.net>
2025-06-08 13:27:52 +0200

    Add test for number input disposer

---
+++ test/browser/createNumberInput.dispose.test.js
@@ -0,0 +1,30 @@
+import { describe, it, expect, jest } from '@jest/globals';
+import { createNumberInput } from '../../src/browser/toys.js';
+
+describe('createNumberInput disposer', () => {
+  it('removes the input event listener when _dispose is called', () => {
+    const mockInput = {};
+    const onChange = jest.fn();
+    let handler;
+    const dom = {
+      createElement: jest.fn(() => mockInput),
+      setType: jest.fn(),
+      setValue: jest.fn(),
+      addEventListener: jest.fn((el, event, h) => {
+        if (event === 'input') {handler = h;}
+      }),
+      removeEventListener: jest.fn(),
+    };
+
+    const input = createNumberInput('1', onChange, dom);
+
+    expect(typeof input._dispose).toBe('function');
+    dom.removeEventListener.mockClear();
+    input._dispose();
+    expect(dom.removeEventListener).toHaveBeenCalledWith(
+      mockInput,
+      'input',
+      handler
+    );
+  });
+});
