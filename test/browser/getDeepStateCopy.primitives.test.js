f5cf280d984d0f302412648a9a3b1f063523af58
Matt <matt@mattheard.net>
2025-06-08 17:45:14 +0200

    Add primitive tests for getDeepStateCopy

---
+++ test/browser/getDeepStateCopy.primitives.test.js
@@ -0,0 +1,8 @@
+import { describe, test, expect } from '@jest/globals';
+import { getDeepStateCopy } from '../../src/browser/toys.js';
+
+describe('getDeepStateCopy primitives', () => {
+  test.each([42, 'abc', true, null])('returns value unchanged for %p', value => {
+    expect(getDeepStateCopy(value)).toBe(value);
+  });
+});
