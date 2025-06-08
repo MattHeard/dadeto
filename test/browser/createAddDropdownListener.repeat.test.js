bab4824caae3259ae1e270027a6c5970ea6ada0c
Matt <matt@mattheard.net>
2025-06-08 15:10:13 +0200

    Add repeat invocation test for createAddDropdownListener

---
+++ test/browser/createAddDropdownListener.repeat.test.js
@@ -0,0 +1,19 @@
+import { describe, it, expect, jest } from '@jest/globals';
+import { createAddDropdownListener } from '../../src/browser/toys.js';
+
+describe('createAddDropdownListener repeated calls', () => {
+  it('registers the handler each time it is invoked', () => {
+    const onChange = jest.fn();
+    const dom = { addEventListener: jest.fn() };
+    const dropdown = {};
+
+    const addListener = createAddDropdownListener(onChange, dom);
+
+    addListener(dropdown);
+    addListener(dropdown);
+
+    expect(dom.addEventListener).toHaveBeenCalledTimes(2);
+    expect(dom.addEventListener).toHaveBeenNthCalledWith(1, dropdown, 'change', onChange);
+    expect(dom.addEventListener).toHaveBeenNthCalledWith(2, dropdown, 'change', onChange);
+  });
+});
