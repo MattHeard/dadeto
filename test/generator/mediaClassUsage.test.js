435c887a4da786509460b6ef963728431933e0c9
Matt <matt@mattheard.net>
2025-06-08 15:29:47 +0200

    Add test for media class

---
+++ test/generator/mediaClassUsage.test.js
@@ -0,0 +1,23 @@
+import { describe, test, expect } from '@jest/globals';
+import { generateBlog } from '../../src/generator/generator.js';
+
+const header = '<body>';
+const footer = '</body>';
+const wrapHtml = c => ['<html>', c, '</html>'].join('');
+
+describe('CLASS.MEDIA usage', () => {
+  test('youtube media section uses media key class', () => {
+    const blog = {
+      posts: [
+        {
+          key: 'VID1',
+          title: 'Video Post',
+          publicationDate: '2024-01-01',
+          youtube: { id: 'abc', timestamp: 0, title: 'Video' },
+        },
+      ],
+    };
+    const html = generateBlog({ blog, header, footer }, wrapHtml);
+    expect(html).toContain('<div class="key media">video</div>');
+  });
+});
