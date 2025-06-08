b0f5de809080e44a364e13c8cb9c49bccdf7a1bc
Matt <matt@mattheard.net>
2025-06-08 17:45:11 +0200

    Add test for blockquote corners on multiple posts

---
+++ test/generator/blockquoteCorners.multiple.test.js
@@ -0,0 +1,31 @@
+import { describe, test, expect } from '@jest/globals';
+import { generateBlog } from '../../src/generator/generator.js';
+
+const header = '<body>';
+const footer = '</body>';
+const wrapHtml = c => ['<html>', c, '</html>'].join('');
+
+describe('BLOCKQUOTE_CORNERS multiple posts', () => {
+  test('blockquote corners appear for each quoted post', () => {
+    const blog = {
+      posts: [
+        {
+          key: 'BQ1',
+          title: 'Quote1',
+          publicationDate: '2024-06-03',
+          content: [{ type: 'quote', content: 'Hi' }],
+        },
+        {
+          key: 'BQ2',
+          title: 'Quote2',
+          publicationDate: '2024-06-04',
+          content: [{ type: 'quote', content: 'Bye' }],
+        },
+      ],
+    };
+
+    const html = generateBlog({ blog, header, footer }, wrapHtml);
+    const matches = html.match(/corner-/g) || [];
+    expect(matches).toHaveLength(8);
+  });
+});
