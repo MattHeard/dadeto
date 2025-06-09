7eeae8d669201de1ad3b78a48907a5c2e3c04dfb
Matt <matt@mattheard.net>
2025-06-09 15:15:41 +0200

    Add test for alternate date formatting

---
+++ test/generator/dateFormat.additional.test.js
@@ -0,0 +1,21 @@
+import { test, expect } from '@jest/globals';
+import { generateBlog } from '../../src/generator/generator.js';
+
+const header = '<body>';
+const footer = '</body>';
+const wrapHtml = html => html;
+
+test('generateBlog formats another publication date with short month', () => {
+  const blog = {
+    posts: [
+      {
+        key: 'DATE2',
+        title: 'Another Date',
+        publicationDate: '2025-12-31',
+        content: ['entry'],
+      },
+    ],
+  };
+  const html = generateBlog({ blog, header, footer }, wrapHtml);
+  expect(html).toContain('31 Dec 2025');
+});
