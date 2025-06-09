69583af40080538743d76eb18e8694cf7577789c
Matt <matt@mattheard.net>
2025-06-09 14:11:03 +0200

    test: verify audio controls attribute

---
+++ test/generator/mediaContentConfig.controls.test.js
@@ -0,0 +1,23 @@
+import { describe, test, expect } from '@jest/globals';
+import { generateBlog } from '../../src/generator/generator.js';
+
+const header = '<body>';
+const footer = '</body>';
+const wrapHtml = c => ['<html>', c, '</html>'].join('');
+
+describe('MEDIA_CONTENT_CONFIG audio controls', () => {
+  test('audio elements include the controls attribute', () => {
+    const blog = {
+      posts: [
+        {
+          key: 'AUDCTRL',
+          title: 'Audio Controls',
+          publicationDate: '2024-06-02',
+          audio: { fileType: 'mp3' },
+        },
+      ],
+    };
+    const html = generateBlog({ blog, header, footer }, wrapHtml);
+    expect(html).toContain('<audio class="value" controls>');
+  });
+});
