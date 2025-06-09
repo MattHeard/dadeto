c046211014f4d7979165f6d6a6456ba57e61ee55
Matt <matt@mattheard.net>
2025-06-09 14:11:05 +0200

    Add whitespace-trimming test for cyberpunkAdventure

---
+++ test/toys/2025-03-30/cyberpunkAdventure.trim.test.js
@@ -0,0 +1,18 @@
+import { describe, test, expect } from '@jest/globals';
+import { cyberpunkAdventure } from '../../../src/toys/2025-03-30/cyberpunkAdventure.js';
+
+describe('cyberpunkAdventure input trimming', () => {
+  test('trims whitespace around commands', () => {
+    let tempData = {};
+    const env = new Map([
+      ['getRandomNumber', () => 0.5],
+      ['getCurrentTime', () => '00:00'],
+      ['getData', () => ({ temporary: { CYBE1: tempData } })],
+      ['setData', data => { tempData = { ...tempData, ...data.temporary?.CYBE1 }; }],
+    ]);
+
+    cyberpunkAdventure('Blaze', env);
+    const result = cyberpunkAdventure('   start  ', env);
+    expect(result).toMatch(/Neon Market/);
+  });
+});
