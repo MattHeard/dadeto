82f38401d87b3ca6201917f7eddbe189a22335a0
Matt <matt@mattheard.net>
2025-06-09 15:42:51 +0200

    Update UUID toy post and cleanup

---
+++ test/toys/2025-06-09/uuid.test.js
@@ -0,0 +1,11 @@
+import { uuidToy } from '../../../src/toys/2025-06-09/uuid.js';
+import { jest } from '@jest/globals';
+
+describe('uuidToy', () => {
+  it('returns the uuid from the environment', () => {
+    const mockUuid = '84f8e1f2-3e9d-4fd1-b1a2-d9c2bd34fdba';
+    const getUuid = jest.fn(() => mockUuid);
+    const env = { get: key => (key === 'getUuid' ? getUuid : undefined) };
+    expect(uuidToy(undefined, env)).toBe(mockUuid);
+  });
+});
