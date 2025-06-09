a85044070ad7ae8025efb08720f9cf2c70c4b097
Matt <matt@mattheard.net>
2025-06-09 14:11:05 +0200

    Add test for blockquote corners placement

---
+++ test/generator/blockquoteCorners.start.test.js
@@ -0,0 +1,23 @@
+import { describe, test, expect } from '@jest/globals';
+import { generateBlog } from '../../src/generator/generator.js';
+
+const header = '<body>';
+const footer = '</body>';
+const wrapHtml = c => ['<html>', c, '</html>'].join('');
+
+describe('BLOCKQUOTE_CORNERS placement', () => {
+  test('blockquote corners appear immediately after blockquote tag', () => {
+    const blog = {
+      posts: [
+        {
+          key: 'BQSTART',
+          title: 'Quote Start',
+          publicationDate: '2024-06-05',
+          content: [{ type: 'quote', content: 'Hi there' }]
+        }
+      ]
+    };
+    const html = generateBlog({ blog, header, footer }, wrapHtml);
+    expect(html).toContain('<blockquote class="value"><div class="corner corner-tl"');
+  });
+});
