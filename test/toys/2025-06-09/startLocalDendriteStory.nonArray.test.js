5b5917c25cd9a94c82c9e76d53685fe8bd6864e1
Matt <matt@mattheard.net>
2025-06-10 09:26:50 +0200

    test: cover non-array DEND1 case

---
+++ test/toys/2025-06-09/startLocalDendriteStory.nonArray.test.js
@@ -0,0 +1,14 @@
+import { startLocalDendriteStory } from '../../../src/toys/2025-06-09/startLocalDendriteStory.js';
+import { test, expect, jest } from '@jest/globals';
+
+test('startLocalDendriteStory replaces non-array DEND1', () => {
+  const env = new Map([
+    ['getUuid', () => 'id-1'],
+    ['getData', () => ({ temporary: { DEND1: 42 } })],
+    ['setData', jest.fn()],
+  ]);
+  const result = JSON.parse(startLocalDendriteStory('{"title":"t","content":"c"}', env));
+  const expected = { id: 'id-1', title: 't', content: 'c', options: [] };
+  expect(result).toEqual(expected);
+  expect(env.get('setData')).toHaveBeenCalledWith({ temporary: { DEND1: [expected] } });
+});
