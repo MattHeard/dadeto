2a893d7f52a1ee70e27a288206349cbcda41bc38
Matt <matt@mattheard.net>
2025-06-10 11:37:54 +0200

    Remove duplicate unit tests

--- test/generator/defaultKeyExtraClasses.mutant.test.js
+++
@@ -1,9 +0,0 @@
-import { describe, it, expect } from '@jest/globals';
-import { defaultKeyExtraClasses } from '../../src/generator/generator.js';
-
-describe('defaultKeyExtraClasses mutant', () => {
-  it('initializes undefined keyExtraClasses to empty string', () => {
-    const result = defaultKeyExtraClasses({});
-    expect(result.keyExtraClasses).toBe('');
-  });
-});
