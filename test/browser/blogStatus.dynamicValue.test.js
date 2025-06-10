68b2052bd844d1786319526cd095a2e23feca628
Matt <matt@mattheard.net>
2025-06-10 11:37:47 +0200

    Remove redundant blog status tests

--- test/browser/blogStatus.dynamicValue.test.js
+++
@@ -1,8 +0,0 @@
-import { describe, it, expect } from '@jest/globals';
-import { shouldCopyStateForFetch } from '../../src/browser/data.js';
-
-describe('BLOG_STATUS dynamic value check', () => {
-  it('shouldCopyStateForFetch returns true for idle status', () => {
-    expect(shouldCopyStateForFetch('idle')).toBe(true);
-  });
-});
