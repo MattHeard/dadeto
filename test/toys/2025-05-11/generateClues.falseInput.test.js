044699298f196d36d1b4454f8c97ab7da1d608a9
Matt <matt@mattheard.net>
2025-06-10 22:03:18 +0200

    Remove duplicate tests and consolidate

--- test/toys/2025-05-11/generateClues.falseInput.test.js
+++ test/toys/2025-05-11/generateClues.falseInput.test.js
@@ -1,8 +1,11 @@
 import { test, expect } from '@jest/globals';
 import { generateClues } from '../../../src/toys/2025-05-11/battleshipSolitaireClues.js';
 
-test('generateClues gracefully handles "false" input', () => {
-  const call = () => generateClues('false');
+test.each([
+  ['false'],
+  ['null'],
+])('generateClues gracefully handles %s input', value => {
+  const call = () => generateClues(value);
   expect(call).not.toThrow();
   expect(call()).toBe('{"error":"Invalid fleet structure"}');
 });
