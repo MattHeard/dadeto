8281ef8edcafd9de4371b9867f9c9f46254e097a
Matt <matt@mattheard.net>
2025-06-13 20:49:15 +0200

    test: ensure dendrite field wrappers use div

--- test/inputHandlers/dendriteStoryHandler.formElement.test.js
+++ test/inputHandlers/dendriteStoryHandler.formElement.test.js
@@ -30,4 +30,34 @@ describe('dendriteStoryHandler form element', () => {
     const firstCall = dom.createElement.mock.calls[0][0];
     expect(firstCall).toBe('div');
   });
+
+  test('uses a div wrapper for each form field', () => {
+    const dom = {
+      hide: jest.fn(),
+      disable: jest.fn(),
+      querySelector: jest.fn(() => null),
+      removeChild: jest.fn(),
+      createElement: jest.fn(() => ({})),
+      setClassName: jest.fn(),
+      getNextSibling: jest.fn(() => ({})),
+      insertBefore: jest.fn(),
+      setType: jest.fn(),
+      setPlaceholder: jest.fn(),
+      setTextContent: jest.fn(),
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      appendChild: jest.fn(),
+      getValue: jest.fn(() => '{}'),
+      setValue: jest.fn(),
+    };
+    const container = {};
+    const textInput = { value: '{}' };
+
+    dendriteStoryHandler(dom, container, textInput);
+
+    const divCalls = dom.createElement.mock.calls.filter(
+      ([tag]) => tag === 'div'
+    );
+    expect(divCalls).toHaveLength(7);
+  });
 });
