645e37ee8c8d9f3da8019b6ad4e07662da3bdbeb
Matt <matt@mattheard.net>
2025-06-14 16:41:08 +0200

    test: cover object normalization

---
+++ test/generator/normalizeContentItem.noTypeObject.test.js
@@ -0,0 +1,23 @@
+import { describe, test, expect } from '@jest/globals';
+import { generateBlog } from '../../src/generator/generator.js';
+
+const header = '<body>';
+const footer = '</body>';
+const wrapHtml = c => c;
+
+describe('normalizeContentItem object without type', () => {
+  test('generateBlog throws when content object lacks type', () => {
+    const blog = {
+      posts: [
+        {
+          key: 'NT1',
+          title: 'No Type',
+          publicationDate: '2025-02-02',
+          content: [{ content: 'missing type' }],
+        },
+      ],
+    };
+    const call = () => generateBlog({ blog, header, footer }, wrapHtml);
+    expect(call).toThrow('renderer is not a function');
+  });
+});
