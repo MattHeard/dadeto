29a453e68252f1e944a73d72ccf4c2e29f532631
Matt <matt@mattheard.net>
2025-07-01 19:42:47 +0200

    Remove unused variable

--- test/browser/toys.createKeyValueRow.test.js
+++ test/browser/toys.createKeyValueRow.test.js
@@ -196,7 +196,6 @@ describe('createKeyValueRow', () => {
     const mockValueInput = {};
     const mockButton = {};
 
-    let inputHandler;
 
     mockDom.createElement
       .mockReturnValueOnce({}) // row div
