5e77f387939a708d4c65bf7668ed04308aa39579
Matt <matt@mattheard.net>
2025-07-01 21:38:36 +0200

    refactor mapValues to reduce complexity

--- src/utils/objectUtils.js
+++ src/utils/objectUtils.js
@@ -22,12 +22,11 @@ export function pick(obj, keys) {
  * @returns {Object} A new object with transformed values
  */
 export function mapValues(obj, fn) {
-  if (!obj || typeof obj !== 'object') {
+  if (Object(obj) !== obj) {
     return {};
   }
 
-  return Object.entries(obj).reduce((result, [key, value]) => {
-    result[key] = fn(value, key);
-    return result;
-  }, {});
+  return Object.fromEntries(
+    Object.entries(obj).map(([key, value]) => [key, fn(value, key)])
+  );
 }
