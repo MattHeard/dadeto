51a018853b8c62cc2a72a584474f5f45c4b28f6b
Matt <matt@mattheard.net>
2025-07-01 21:54:18 +0200

    refactor: replace ternary in sequentialValues test

--- test/browser/createInputDropdownHandler.sequentialValues.test.js
+++ test/browser/createInputDropdownHandler.sequentialValues.test.js
@@ -10,7 +10,12 @@ test('createInputDropdownHandler handles text, unknown, then text', () => {
   const dom = {
     getCurrentTarget: jest.fn(() => select),
     getParentElement: jest.fn(() => container),
-    querySelector: jest.fn((_, selector) => (selector === 'input[type="text"]' ? textInput : null)),
+    querySelector: jest.fn((_, selector) => {
+      if (selector === 'input[type="text"]') {
+        return textInput;
+      }
+      return null;
+    }),
     getValue: jest
       .fn()
       .mockReturnValueOnce('text')
