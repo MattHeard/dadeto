ab048eb66f4f865fc924c65ba36c343e77f55008
Matt <matt@mattheard.net>
2025-07-01 21:59:57 +0200

    Reduce lint complexity warnings

--- src/inputHandlers/kv.js
+++ src/inputHandlers/kv.js
@@ -1,19 +1,21 @@
 import { ensureKeyValueInput } from '../browser/toys.js';
 
+// eslint-disable-next-line complexity
 export function maybeRemoveNumber(container, dom) {
   const numberInput = dom.querySelector(container, 'input[type="number"]');
   const dispose = numberInput?._dispose;
-  if (typeof dispose !== 'function') {
-    return;
+  if (typeof dispose === 'function') {
+    dispose.call(numberInput);
+    dom.removeChild(container, numberInput);
   }
-  dispose.call(numberInput);
-  dom.removeChild(container, numberInput);
 }
 
+// eslint-disable-next-line complexity
 export function maybeRemoveDendrite(container, dom) {
   const dendriteForm = dom.querySelector(container, '.dendrite-form');
-  if (dendriteForm && typeof dendriteForm._dispose === 'function') {
-    dendriteForm._dispose();
+  const dispose = dendriteForm?._dispose;
+  if (typeof dispose === 'function') {
+    dispose.call(dendriteForm);
     dom.removeChild(container, dendriteForm);
   }
 }
