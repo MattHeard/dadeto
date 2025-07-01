8d1c154bdb6cd27c236985468ee1ca8cc521b610
Matt <matt@mattheard.net>
2025-07-01 22:27:00 +0200

    refactor text handler to reuse dispose helper

--- src/inputHandlers/text.js
+++ src/inputHandlers/text.js
@@ -1,19 +1,9 @@
+import { dispose } from './default.js';
+
 export function textHandler(dom, container, textInput) {
   dom.reveal(textInput);
   dom.enable(textInput);
-  const numberInput = dom.querySelector(container, 'input[type="number"]');
-  if (numberInput && typeof numberInput._dispose === 'function') {
-    numberInput._dispose();
-    dom.removeChild(container, numberInput);
-  }
-  const kvContainer = dom.querySelector(container, '.kv-container');
-  if (kvContainer && typeof kvContainer._dispose === 'function') {
-    kvContainer._dispose();
-    dom.removeChild(container, kvContainer);
-  }
-  const dendriteForm = dom.querySelector(container, '.dendrite-form');
-  if (dendriteForm && typeof dendriteForm._dispose === 'function') {
-    dendriteForm._dispose();
-    dom.removeChild(container, dendriteForm);
-  }
+  dispose(dom.querySelector(container, 'input[type="number"]'), dom, container);
+  dispose(dom.querySelector(container, '.kv-container'), dom, container);
+  dispose(dom.querySelector(container, '.dendrite-form'), dom, container);
 }
