afbd7112b2afb0dd19ef2c8551d6b0ee490ae949
Matt <matt@mattheard.net>
2025-07-02 21:07:55 +0200

    Remove direct locale spy

--- test/generator/generateBlogOuter.dateFormat.test.js
+++ test/generator/generateBlogOuter.dateFormat.test.js
@@ -1,4 +1,4 @@
-import { describe, it, expect, jest } from '@jest/globals';
+import { describe, it, expect } from '@jest/globals';
 import { generateBlogOuter } from '../../src/generator/generator.js';
 
 describe('generateBlogOuter date formatting', () => {
@@ -14,10 +14,7 @@ describe('generateBlogOuter date formatting', () => {
       ],
     };
 
-    const spy = jest.spyOn(Date.prototype, 'toLocaleDateString');
     const html = generateBlogOuter(blog);
     expect(html).toContain('<p class="value metadata">1 Jun 2024</p>');
-    expect(spy).toHaveBeenCalledWith('en-GB', expect.any(Object));
-    spy.mockRestore();
   });
 });
