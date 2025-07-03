d832e1450f887ec6d9a25a4b9df00c3612866c27
Matt <matt@mattheard.net>
2025-07-03 13:49:44 +0200

    Add JSDoc to escapeWithReplacements

--- test/generator/html.replacements.integrity.test.js
+++ test/generator/html.replacements.integrity.test.js
@@ -1,8 +1,17 @@
 import { describe, test, expect } from '@jest/globals';
 import { htmlEscapeReplacements } from '../../src/generator/html.js';
 
+/**
+ * Replace characters in a string using a set of replacements.
+ * @param {string} text - Input to escape.
+ * @param {{from: RegExp, to: string}[]} replacements - Patterns to replace.
+ * @returns {string} Escaped string.
+ */
 function escapeWithReplacements(text, replacements) {
-  return replacements.reduce((acc, { from, to }) => acc.replace(from, to), text);
+  return replacements.reduce(
+    (acc, { from, to }) => acc.replace(from, to),
+    text
+  );
 }
 
 describe('HTML_ESCAPE_REPLACEMENTS integrity', () => {
