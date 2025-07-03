b287c9a2e522b6abd949df5c9314f6ca871f003d
Matt <matt@mattheard.net>
2025-07-03 13:49:44 +0200

    Add JSDoc comments for isObject helper

--- src/browser/common.js
+++ src/browser/common.js
@@ -9,6 +9,12 @@ function isNonNullNonArray(val) {
   return val !== null && !Array.isArray(val);
 }
 
+/**
+ * Determine if a value is an object that is not null or an array.
+ *
+ * @param {*} val - Value to test.
+ * @returns {boolean} True when `val` is an object.
+ */
 export function isObject(val) {
   return isNonNullNonArray(val) && typeof val === 'object';
 }
