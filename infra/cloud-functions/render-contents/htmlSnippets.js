5a4908a2f85fe4308acc898e6b444d3492ff9797
Matt <matt@mattheard.net>
2025-07-31 23:34:37 +0200

    Add Pico CSS stylesheet link to Dendrite HTML

--- infra/cloud-functions/render-contents/htmlSnippets.js
+++ infra/cloud-functions/render-contents/htmlSnippets.js
@@ -2,4 +2,4 @@ export const LIST_ITEM_HTML = (pageNumber, title) =>
   `<li><a href="./p/${pageNumber}a.html">${title}</a></li>`;
 
 export const PAGE_HTML = list =>
-  `<!doctype html><html lang="en"><head><meta charset="UTF-8" /><meta name="viewport" content="width=device-width, initial-scale=1" /><title>Dendrite</title></head><body><h1><a href="/">Dendrite</a></h1><h2>Contents</h2><ol>${list}</ol></body></html>`;
+  `<!doctype html><html lang="en"><head><meta charset="UTF-8" /><meta name="viewport" content="width=device-width, initial-scale=1" /><title>Dendrite</title><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.fluid.classless.min.css" /></head><body><h1><a href="/">Dendrite</a></h1><h2>Contents</h2><ol>${list}</ol></body></html>`;
