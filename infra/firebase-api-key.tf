10078e3ee6aa30ce5f3b97039677d690dfc110b6
Matt <matt@mattheard.net>
2025-08-05 20:09:37 +0200

    Update API keys admin role for Terraform

--- infra/firebase-api-key.tf
+++ infra/firebase-api-key.tf
@@ -7,7 +7,7 @@
 # Allow Terraform to create / update / delete API keys
 resource "google_project_iam_member" "terraform_apikeys_admin" {
   project = var.project_id
-  role    = "roles/serviceusage.apiKeysAdmin"
+  role    = "roles/apikeys.admin"
   member  = "serviceAccount:terraform@${var.project_id}.iam.gserviceaccount.com"
 }
 
