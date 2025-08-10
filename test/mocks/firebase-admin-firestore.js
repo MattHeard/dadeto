04dd339a55f54f0b61ecedde7190aadeb101cca8
Matt <matt@mattheard.net>
2025-08-10 14:11:49 +0200

    feat: purge CDN cache for rendered variants

--- test/mocks/firebase-admin-firestore.js
+++ test/mocks/firebase-admin-firestore.js
@@ -1,1 +1,12 @@
+import { jest } from '@jest/globals';
+
 export const FieldValue = { delete: () => {} };
+
+export const mockDoc = jest.fn();
+
+/**
+ *
+ */
+export function getFirestore() {
+  return { doc: mockDoc };
+}
