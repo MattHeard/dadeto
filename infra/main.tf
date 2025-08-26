554923f33674e37191a372cb6a5b0a9ea3b68d0f
Matt <matt@mattheard.net>
2025-08-26 14:31:07 +0900

    Move Dendrite about page into infra Terraform

--- infra/main.tf
+++ infra/main.tf
@@ -79,6 +79,13 @@ resource "google_storage_bucket_object" "dendrite_new_page" {
   content_type = "text/html"
 }
 
+resource "google_storage_bucket_object" "dendrite_about" {
+  name         = "about.html"
+  bucket       = google_storage_bucket.dendrite_static.name
+  source       = "${path.module}/about.html"
+  content_type = "text/html"
+}
+
 resource "google_storage_bucket_object" "dendrite_mod" {
   name   = "mod.html"
   bucket = google_storage_bucket.dendrite_static.name
