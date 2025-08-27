4f0cf8ad084a9aa7688da6d88a2df7442d3c7450
Matt <matt@mattheard.net>
2025-08-27 14:39:09 +0900

    Refactor cloud function trigger handling

--- infra/modules/cloud-function/outputs.tf
+++ infra/modules/cloud-function/outputs.tf
@@ -3,5 +3,6 @@ output "name" {
 }
 
 output "https_trigger_url" {
-  value = google_cloudfunctions_function.function.https_trigger_url
+  # null when not HTTP; your scheduler reference will still work for HTTP funcs
+  value = local.trigger_http ? google_cloudfunctions_function.function.https_trigger_url : null
 }
