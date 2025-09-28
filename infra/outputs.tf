03b5662c3b106c604c5830afeb31babb0e0608bd
Matt <matt@mattheard.net>
2025-09-28 10:37:12 +0200

    Use tenant resource name for Firebase tenant outputs

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -5,7 +5,7 @@ output "firebase_web_app_config" {
 output "identity_platform_tenant" {
   value = {
     name       = google_identity_platform_tenant.environment.name
-    tenant_id  = google_identity_platform_tenant.environment.tenant_id
+    id         = google_identity_platform_tenant.environment.id
     project_id = var.project_id
   }
 }
