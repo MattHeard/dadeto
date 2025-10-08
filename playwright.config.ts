99a8bee1ab68b0e0039198569ea33f53f14ba6b2
Matt <matt@mattheard.net>
2025-10-08 17:09:24 +0200

    Add chromium launch flags for Playwright tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from '@playwright/test';
+import { chromiumProject } from './test/e2e/playwright.config';
 
 export default defineConfig({
   testDir: './test/e2e',
@@ -6,7 +7,9 @@ export default defineConfig({
     ['list'],
     ['html', { outputFolder: 'playwright-report', open: 'never' }],
   ],
+  projects: [chromiumProject],
   use: {
+    ...chromiumProject.use,
     trace: 'retain-on-failure',
     screenshot: 'on',
   },
