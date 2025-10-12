387c0abb4be1d748ee29f985644eecec2ff51f09
Matt <matt@mattheard.net>
2025-10-12 12:41:29 +0200

    Ensure Playwright proxy resources wait for network IAM

--- infra/playwright.tf
+++ infra/playwright.tf
@@ -40,6 +40,8 @@ resource "google_compute_subnetwork" "playwright_proxy_only" {
   ip_cidr_range = var.playwright_proxy_subnet_cidr
   purpose       = "REGIONAL_MANAGED_PROXY"
   role          = "ACTIVE"
+
+  depends_on = [google_project_iam_member.terraform_service_account_network_roles]
 }
 
 resource "google_vpc_access_connector" "playwright" {
@@ -51,6 +53,7 @@ resource "google_vpc_access_connector" "playwright" {
   ip_cidr_range = "10.8.1.0/28"
   depends_on = [
     google_project_service.playwright_vpc_access,
+    google_project_iam_member.terraform_service_account_network_roles,
   ]
 }
 
@@ -117,6 +120,8 @@ resource "google_cloud_run_v2_service" "gcs_proxy" {
   }
 
   ingress = "INGRESS_TRAFFIC_INTERNAL_LOAD_BALANCER"
+
+  depends_on = [google_project_iam_member.terraform_service_account_network_roles]
 }
 
 resource "google_cloud_run_v2_service_iam_member" "gcs_proxy_unauth" {
@@ -177,6 +182,8 @@ resource "google_compute_address" "gcs_proxy_ilb_ip" {
   address_type = "INTERNAL"
   purpose      = "GCE_ENDPOINT"
   subnetwork   = data.google_compute_subnetwork.playwright[0].id
+
+  depends_on = [google_project_iam_member.terraform_service_account_network_roles]
 }
 
 resource "google_compute_forwarding_rule" "gcs_proxy_ilb_fw" {
@@ -191,6 +198,8 @@ resource "google_compute_forwarding_rule" "gcs_proxy_ilb_fw" {
   network               = data.google_compute_network.playwright[0].id
   subnetwork            = data.google_compute_subnetwork.playwright[0].id
   ip_address            = google_compute_address.gcs_proxy_ilb_ip[0].address
+
+  depends_on = [google_project_iam_member.terraform_service_account_network_roles]
 }
 
 resource "google_storage_bucket" "e2e_reports" {
