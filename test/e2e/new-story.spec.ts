547bb4889eba4433e681635ce9a86e2fbf1811b6
Matt <matt@mattheard.net>
2025-10-14 08:15:31 +0200

    Remove BASE_URL assertion from e2e spec

--- test/e2e/new-story.spec.ts
+++ test/e2e/new-story.spec.ts
@@ -1,12 +1,6 @@
 import { test, expect } from '@playwright/test';
 
 test('serves new-story.html through the proxy', async ({ page }) => {
-  const baseUrl = test.info().project.use.baseURL;
-  if (!baseUrl) {
-    test.skip(true, 'BASE_URL must be configured to reach the proxy service');
-    return;
-  }
-
   const response = await page.goto('/new-story.html', {
     waitUntil: 'domcontentloaded',
   });
