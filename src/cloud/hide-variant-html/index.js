f75a2a324d1f67221edc5c3bc6fcb5ad1199bd07
Matt <matt@mattheard.net>
2025-10-21 11:30:14 +0200

    Add cloud removeVariantHtml re-export wrapper

--- src/cloud/hide-variant-html/index.js
+++ src/cloud/hide-variant-html/index.js
@@ -1,6 +1,6 @@
 import * as functions from 'firebase-functions/v1';
 import { Storage } from '@google-cloud/storage';
-import { createRemoveVariantHtml } from '../../core/cloud/hide-variant-html/removeVariantHtml.js';
+import { createRemoveVariantHtml } from './removeVariantHtml.js';
 import { ensureFirebaseApp } from './firebaseApp.js';
 
 ensureFirebaseApp();
