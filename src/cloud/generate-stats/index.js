d5bf2b70150e3a25645c61e379441c6bd392ac5c
Matt <matt@mattheard.net>
2025-10-24 09:16:15 +0200

    Inline generate stats allowed origins helper

--- src/cloud/generate-stats/index.js
+++ src/cloud/generate-stats/index.js
@@ -4,11 +4,31 @@ import * as functions from 'firebase-functions/v1';
 import express from 'express';
 import cors from 'cors';
 import { getAuth } from 'firebase-admin/auth';
-import { getAllowedOrigins } from './cors-config.js';
 import { ensureFirebaseApp } from './firebaseApp.js';
 import { getFirestoreInstance } from './firestore.js';
 import { createGenerateStatsCore } from './core.js';
 import { fetchFn } from './gcf.js';
+
+const productionOrigins = [
+  'https://mattheard.net',
+  'https://dendritestories.co.nz',
+  'https://www.dendritestories.co.nz',
+];
+
+const getAllowedOrigins = (environmentVariables) => {
+  const environment = environmentVariables?.DENDRITE_ENVIRONMENT;
+  const playwrightOrigin = environmentVariables?.PLAYWRIGHT_ORIGIN;
+
+  if (environment === 'prod') {
+    return productionOrigins;
+  }
+
+  if (typeof environment === 'string' && environment.startsWith('t-')) {
+    return playwrightOrigin ? [playwrightOrigin] : [];
+  }
+
+  return productionOrigins;
+};
 
 const getProcessEnv = () => process.env;
 
