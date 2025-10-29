d5c7844434eb068b74fe0f4f73ea7884fe554f5f
Matt <matt@mattheard.net>
2025-10-29 09:04:07 +0100

    Move production origins constant into cloud core

--- src/core/common-core.js
+++ src/core/common-core.js
@@ -1,15 +1,4 @@
 /**
- * Origins that are permitted to access production endpoints.
- * These are reused by both cloud functions and other integrations
- * that need to reason about the live deployment.
- */
-export const productionOrigins = [
-  'https://mattheard.net',
-  'https://dendritestories.co.nz',
-  'https://www.dendritestories.co.nz',
-];
-
-/**
  * Default storage bucket used for production content.
  * Shared so non-Cloud systems can mirror the same configuration.
  */
