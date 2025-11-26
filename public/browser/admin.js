abb065c89c3f44d9b91aa2f965c642345b07c86a
Matthew Heard <matthew.heard@outfittery.de>
2025-11-26 09:51:17 +0100

    refactor

--- public/browser/admin.js
+++ public/browser/admin.js
@@ -1,10 +1,18 @@
-import * as googleAuth from './googleAuth.js';
+import { initGoogleSignIn, signOut } from './googleAuth.js';
 import { loadStaticConfig } from './loadStaticConfig.js';
 import { initAdmin } from './admin-core.js';
 import {
   getAuth,
   onAuthStateChanged,
 } from 'https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js';
+
+import { getIdToken } from '../core/browser/browser-core.js';
+
+const googleAuth = {
+  initGoogleSignIn,
+  signOut,
+  getIdToken,
+};
 
 initAdmin({
   googleAuthModule: googleAuth,
