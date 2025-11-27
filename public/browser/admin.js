a3d2f28fa7367fca4b3a79ab35445f608fb1bffa
Matthew Heard <matthew.heard@outfittery.de>
2025-11-27 13:41:05 +0100

    refactor

--- public/browser/admin.js
+++ public/browser/admin.js
@@ -14,48 +14,50 @@ import {
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js';
 import { getIdToken } from '../core/browser/browser-core.js';
 
-setupFirebase(initializeApp);
+(() => {
+  setupFirebase(initializeApp);
 
-let initGoogleSignInHandler;
-const getInitGoogleSignInHandler = () => {
-  if (!initGoogleSignInHandler) {
-    const auth = getAuth();
-    initGoogleSignInHandler = createGoogleSignInInit(
-      auth,
-      sessionStorage,
-      console,
-      globalThis,
-      GoogleAuthProvider,
-      signInWithCredential
-    );
-  }
-  return initGoogleSignInHandler;
-};
+  let initGoogleSignInHandler;
+  const getInitGoogleSignInHandler = () => {
+    if (!initGoogleSignInHandler) {
+      const auth = getAuth();
+      initGoogleSignInHandler = createGoogleSignInInit(
+        auth,
+        sessionStorage,
+        console,
+        globalThis,
+        GoogleAuthProvider,
+        signInWithCredential
+      );
+    }
+    return initGoogleSignInHandler;
+  };
 
-const initGoogleSignIn = options => getInitGoogleSignInHandler()(options);
+  const initGoogleSignIn = options => getInitGoogleSignInHandler()(options);
 
-let signOutHandler;
-const getSignOutHandler = () => {
-  if (!signOutHandler) {
-    const auth = getAuth();
-    signOutHandler = createSignOut(auth, globalThis);
-  }
-  return signOutHandler;
-};
+  let signOutHandler;
+  const getSignOutHandler = () => {
+    if (!signOutHandler) {
+      const auth = getAuth();
+      signOutHandler = createSignOut(auth, globalThis);
+    }
+    return signOutHandler;
+  };
 
-const signOut = () => getSignOutHandler()();
+  const signOut = () => getSignOutHandler()();
 
-const googleAuth = {
-  initGoogleSignIn,
-  signOut,
-  getIdToken,
-};
+  const googleAuth = {
+    initGoogleSignIn,
+    signOut,
+    getIdToken,
+  };
 
-initAdmin({
-  googleAuthModule: googleAuth,
-  loadStaticConfigFn: loadStaticConfig,
-  getAuthFn: getAuth,
-  onAuthStateChangedFn: onAuthStateChanged,
-  doc: document,
-  fetchFn: fetch,
-});
+  initAdmin({
+    googleAuthModule: googleAuth,
+    loadStaticConfigFn: loadStaticConfig,
+    getAuthFn: getAuth,
+    onAuthStateChangedFn: onAuthStateChanged,
+    doc: document,
+    fetchFn: fetch,
+  });
+})();
