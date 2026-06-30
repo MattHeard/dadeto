3be4b1d80da363aadc473504dcb212648c6dec4b
Matt Heard <matt@mattheard.net>
2026-06-30 07:30:37 +0200

    Complete update-variant-visibility wrapper exports

--- src/cloud/update-variant-visibility/update-variant-visibility-gcf.js
+++ src/cloud/update-variant-visibility/update-variant-visibility-gcf.js
@@ -1,6 +1,10 @@
 import functions from './firebase-functions.js';
 
 export { functions };
+export { Storage } from '@google-cloud/storage';
+export { getAuth } from 'firebase-admin/auth';
+export { createFirebaseAppManager } from './common-gcf.js';
 export { getFirestoreInstance } from './firestore.js';
 export { ADMIN_UID } from '../../core/commonCore.js';
+export { fetchFn, crypto, getEnvironmentVariables } from './common-gcf.js';
 export * from './common-gcf.js';
