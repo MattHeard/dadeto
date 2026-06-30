01f1580ef6eb13d41e63995bb9ab7a455290aef1
Matt Heard <matt@mattheard.net>
2026-06-30 07:17:07 +0200

    Fix update-variant-visibility shared core import

--- src/cloud/update-variant-visibility/update-variant-visibility-gcf.js
+++ src/cloud/update-variant-visibility/update-variant-visibility-gcf.js
@@ -2,5 +2,5 @@ import functions from './firebase-functions.js';
 
 export { functions };
 export { getFirestoreInstance } from './firestore.js';
-export { ADMIN_UID } from './common-core.js';
+export { ADMIN_UID } from '../../core/commonCore.js';
 export * from './common-gcf.js';
