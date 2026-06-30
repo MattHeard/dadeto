b45632662a89153aa90bc5a66baf7f8cf2100292
Matt Heard <matt@mattheard.net>
2026-06-30 06:41:11 +0200

    Fix update-variant-visibility exports

--- src/cloud/update-variant-visibility/update-variant-visibility-gcf.js
+++ src/cloud/update-variant-visibility/update-variant-visibility-gcf.js
@@ -2,4 +2,5 @@ import functions from './firebase-functions.js';
 
 export { functions };
 export { getFirestoreInstance } from './firestore.js';
+export { ADMIN_UID } from './common-core.js';
 export * from './common-gcf.js';
