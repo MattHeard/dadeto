f6ad7d6f06a53e028f34e5751b86533a0b992d9a
Matt Heard <matt@mattheard.net>
2026-07-08 13:31:05 +0200

    Remove tracked public output

--- public/core/browser/inputHandlers/fileInputSettings.js
+++
@@ -1,5 +0,0 @@
-export const FILE_INPUT_SETTINGS = {
-  type: 'file',
-  className: 'toy-file-input',
-  accept: '.csv,text/csv,text/plain',
-};
--- public/core/browser/toys/2025-12-05/index.js
+++
@@ -1,2 +0,0 @@
-export { calculateVisibility } from './calculateVisibility.js';
-export const TOYS_2025_12_05_INDEX_MARKER = true;
