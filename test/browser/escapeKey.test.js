a7a7266cfe62bb129676938885a6e0fed42e4271
Matt Heard <matt@mattheard.net>
2026-08-21 15:51:18 +0200

    Strengthen escape key mutation coverage

--- test/browser/escapeKey.test.js
+++ test/browser/escapeKey.test.js
@@ -4,6 +4,7 @@ import { isEscapeKeydown } from '../../src/core/browser/inputHandlers/escapeKey.
 describe('isEscapeKeydown', () => {
   test('detects escape keydown events', () => {
     expect(isEscapeKeydown({ type: 'keydown', key: 'Escape' })).toBe(true);
+    expect(isEscapeKeydown({ type: 'keydown', key: 'Enter' })).toBe(false);
   });
 
   test('rejects non-keydown events', () => {
